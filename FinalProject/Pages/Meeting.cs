06d753a40c792650c7e8de748256f2f98bdd316a
Danylo Raduchych <danil.r87602@gmail.com>
2023-08-30 17:05:57 +0200

    1

---
+++ FinalProject/Pages/Meeting.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProject.Pages
+{
+    public class MeetingModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public MeetingModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
